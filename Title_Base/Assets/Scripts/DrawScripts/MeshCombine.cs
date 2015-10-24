c9ad04da6cfa15eb3ffb2990cb00d391dd6b82fe
SGallwas <sgallwas@gmail.com>
2015-10-23 21:10:12 -0700

    hgjh
    jhfjyhjh

---
+++ Title_Base/Assets/Scripts/DrawScripts/MeshCombine.cs
@@ -0,0 +1,187 @@
+﻿/*********************************
+ * MeshCombine.cs
+ * Troy
+ * Created 10/17/2015
+ * Copyright © 2015 DigiPen Institute of Technology, All Rights Reserved
+ *********************************/
+
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+//[ExecuteInEditMode()]
+[AddComponentMenu("Mesh Combine")]
+public class MeshCombine : MonoBehaviour
+{
+    public string combineName = "Combined Mesh";
+
+    public bool addMeshCollider;
+    public bool destroyAfterwards;
+    public bool getShadows = true;
+    public bool castShadow = true;
+    public bool combineOnStart = true;
+
+    /// Rendering with a triangle list is usually faster, but when there's very few triangles the list takes longer
+    public bool generateTriangles = true;
+
+    public int waitFrame;
+
+    private void Start()
+    {
+        if (combineOnStart && waitFrame == 0)
+        {
+            CombineImmediately();
+        }
+
+        else
+        {
+            StartCoroutine(CombineLate());
+        }
+    }
+
+    [ContextMenu("Start Combining")]
+    public void BeginToCombine()
+    {
+        MeshCombine[] children = gameObject.GetComponentsInChildren<MeshCombine>();
+
+        for (int loop = 0; loop < children.Length; ++loop)
+        {
+            if (this != children[loop])
+            {
+                children[loop].CombineImmediately();
+            }
+        }
+
+        combineOnStart = enabled = false;
+    }
+
+    private IEnumerator CombineLate()
+    {
+        // Just combine them as fast/slow
+        for (int loop = 0; loop < waitFrame; ++loop)
+        {
+            yield return 0;
+        }
+
+        CombineImmediately();
+    }
+
+    /// This option has a far longer preprocessing time at startup but leads to better runtime performance.
+    [ContextMenu("Combine Immediately")]
+    public void CombineImmediately()
+    {
+        MeshFilter[] filterList = GetComponentsInChildren<MeshFilter>();
+        Matrix4x4 pos = transform.worldToLocalMatrix;
+        Dictionary<Material, List<NewMeshInstantiate.MeshInstance>> materialToMesh = new Dictionary<Material, List<NewMeshInstantiate.MeshInstance>>();
+
+        foreach (MeshFilter mfilter in filterList)
+        {
+            Renderer current = mfilter.GetComponent<Renderer>();
+
+            // Don't use inactive meshes
+            if (current == null || !current.enabled)
+            {
+                continue;
+            }
+
+            NewMeshInstantiate.MeshInstance currInst = new NewMeshInstantiate.MeshInstance{};
+            currInst.mesh = mfilter.sharedMesh;
+            currInst.transform = pos * mfilter.transform.localToWorldMatrix;
+
+            // Don't use inactive meshes
+            if (currInst.mesh == null)
+            {
+                continue;
+            }
+
+            Material[] currmaterial = current.sharedMaterials;
+
+            // The mesh is active & valid, let's merge it into the parent
+            for (int loop = 0; loop < currmaterial.Length; ++loop)
+            {
+                currInst.subMeshIndex = Math.Min(loop, currInst.mesh.subMeshCount - 1);
+                List<NewMeshInstantiate.MeshInstance> objects;
+
+                if (!materialToMesh.TryGetValue(currmaterial[loop], out objects))
+                {
+                    objects = new List<NewMeshInstantiate.MeshInstance>();
+                    materialToMesh.Add(currmaterial[loop], objects);
+                }
+
+                objects.Add(currInst);
+            }
+
+            // Trash removal logic
+            if (destroyAfterwards && combineOnStart && Application.isPlaying)
+            {
+                Destroy(current.gameObject);
+            }
+
+            else if (destroyAfterwards)
+            {
+                DestroyImmediate(current.gameObject);
+            }
+
+            else
+            {
+                current.enabled = false;
+            }
+        }
+
+        int meshIndex = 0;
+
+        // Assign the new mesh instances
+        foreach (KeyValuePair<Material, List<NewMeshInstantiate.MeshInstance>> de in materialToMesh)
+        {
+            foreach (NewMeshInstantiate.MeshInstance currInst in de.Value)
+            {
+                currInst.targetIndex = meshIndex;
+            }
+
+            ++meshIndex;
+        }
+
+        // Get its components
+        if (!GetComponent<MeshFilter>())
+        {
+            gameObject.AddComponent<MeshFilter>();
+        }
+
+        MeshFilter filter = GetComponent<MeshFilter>();
+
+        if (!GetComponent<MeshRenderer>())
+        {
+            gameObject.AddComponent<MeshRenderer>();
+        }
+
+        Renderer renderer = GetComponent<MeshRenderer>();
+
+        // Apply the changes in-game
+        Mesh final = NewMeshInstantiate.CombinePrep(materialToMesh.SelectMany(kvp => kvp.Value), generateTriangles);
+        final.name = combineName;
+
+        if (Application.isPlaying)
+        {
+            filter.mesh = final;
+        }
+
+        else
+        {
+            filter.sharedMesh = final;
+        }
+
+        renderer.materials = materialToMesh.Keys.ToArray();
+        renderer.enabled = true;
+
+        // Got through the optional steps
+        if (addMeshCollider)
+        {
+            gameObject.AddComponent<MeshCollider>();
+        }
+
+        renderer.castShadows = castShadow;
+        renderer.receiveShadows = getShadows;
+    }
+}
--- Title_Base/Assets/Scripts/DrawScripts/NewMeshInstantiate.cs
+++ Title_Base/Assets/Scripts/DrawScripts/NewMeshInstantiate.cs
@@ -15,211 +15,211 @@ using System.Linq;
 public class NewMeshInstantiate : MonoBehaviour
 {
 
-    private List<Color> colourList = new List<Color>();
-    private List<Vector3> meshNormals = new List<Vector3>();
-    private Dictionary<int, List<int>> subMeshList = new Dictionary<int, List<int>>();
-    private Dictionary<int, List<int>> triangleList = new Dictionary<int, List<int>>();
+  private List<Color> colourList = new List<Color>();
+  private List<Vector3> meshNormals = new List<Vector3>();
+  private Dictionary<int, List<int>> subMeshList = new Dictionary<int, List<int>>();
+  private Dictionary<int, List<int>> triangleList = new Dictionary<int, List<int>>();
 
-    private List<Vector2> uv = new List<Vector2>();
-    private List<Vector2> uv2 = new List<Vector2>();
-    private List<Vector3> vertexList = new List<Vector3>();
-    private List<Vector4> tangentList = new List<Vector4>();
-    private bool isTriangles;
+  private List<Vector2> uv = new List<Vector2>();
+  private List<Vector2> uv2 = new List<Vector2>();
+  private List<Vector3> vertexList = new List<Vector3>();
+  private List<Vector4> tangentList = new List<Vector4>();
+  private bool isTriangles;
 
-    // I guess this is how classes within classwa works?
-    #region Nested type: MeshInstance
+  // I guess this is how classes within classes works?
+  #region Nested type: MeshInstance
 
-    public class MeshInstance
+  public class MeshInstance
+  {
+    public Mesh mesh;
+    public int subMeshIndex;
+    public int targetIndex;
+    public Matrix4x4 transform;
+  }
+
+  #endregion
+
+  public NewMeshInstantiate(bool trianglesOrList)
+  {
+    // Are we using just triangles or were they assorted into a List?
+    isTriangles = trianglesOrList;
+  }
+
+  public void VertexStart(int vertices)
+  {
+    // Determine how many vertices CombineMeshes would've missed
+    int toPlug = vertices - (vertexList.Capacity - vertexList.Count);
+
+    // Plug them in manually
+    colourList.Capacity += toPlug;
+    meshNormals.Capacity += toPlug;
+    uv.Capacity += toPlug;
+    uv2.Capacity += toPlug;
+    vertexList.Capacity += toPlug;
+    tangentList.Capacity += toPlug;
+  }
+
+  public void TriangleStart(int targetIndex, int indices)
+  {
+    // Don't add triangles if we're not using a triangle list!
+    if (!triangleList.ContainsKey(targetIndex))
     {
-        public Mesh mesh;
-        public int subMeshIndex;
-        public int targetIndex;
-        public Matrix4x4 transform;
+      triangleList.Add(targetIndex, new List<int>());
     }
 
-    #endregion
+    // Determine how many triangles CombineMeshes would've missed and plug them in
+    int toPlug = indices - (triangleList[targetIndex].Capacity - triangleList[targetIndex].Count);
+    triangleList[targetIndex].Capacity += toPlug;
+  }
 
-    public NewMeshInstantiate(bool trianglesOrList)
+  public void AddMeshStart(int targetIndex, IEnumerable<int> lengthList)
+  {
+    // Don't add meshs if we're not using a triangle list!
+    if (!subMeshList.ContainsKey(targetIndex))
     {
-        // Are we using just triangles or were they assorted into a List?
-        isTriangles = trianglesOrList;
-    }
-    
-    public void VertexStart(int vertices)
-    {
-        // Determine how many vertices CombineMeshes would've missed
-        int toPlug = vertices - (vertexList.Capacity - vertexList.Count);
-
-        // Plug them in manually
-        colourList.Capacity += toPlug;
-        meshNormals.Capacity += toPlug;
-        uv.Capacity += toPlug;
-        uv2.Capacity += toPlug;
-        vertexList.Capacity += toPlug;
-        tangentList.Capacity += toPlug;
+      subMeshList.Add(targetIndex, new List<int>());
     }
 
-    public void TriangleStart(int targetIndex, int indices)
+    // This is how much space we're using now.  This could very well change!
+    int requiredCapacity = subMeshList[targetIndex].Count;
+
+    foreach (int srcLength in lengthList)
     {
-        // Don't add triangles if we're not using a triangle list!
-        if (!triangleList.ContainsKey(targetIndex))
+      int adjLength = srcLength;
+
+      // Determine the length of the meshes we're adding in memory size and add them
+      if (requiredCapacity > 0)
+      {
+        if ((requiredCapacity & 1) == 1)
         {
-            triangleList.Add(targetIndex, new List<int>());
-        }
-
-        // Determine how many triangles CombineMeshes would've missed and plug them in
-        int toPlug = indices - (triangleList[targetIndex].Capacity - triangleList[targetIndex].Count);
-        triangleList[targetIndex].Capacity += toPlug;
-    }
-
-    public void AddMeshStart(int targetIndex, IEnumerable<int> lengthList)
-    {
-        // Don't add meshs if we're not using a triangle list!
-        if (!subMeshList.ContainsKey(targetIndex))
-        {
-            subMeshList.Add(targetIndex, new List<int>());
-        }
-
-        // This is how much space we're using now.  This could very well change!
-        int requiredCapacity = subMeshList[targetIndex].Count;
-
-        foreach (int srcLength in lengthList)
-        {
-            int adjLength = srcLength;
-
-            // Determine the length of the meshes we're adding in memory size and add them
-            if (requiredCapacity > 0)
-            {
-                if ((requiredCapacity & 1) == 1)
-                {
-                    adjLength += 3;
-                }
-                else
-                {
-                    adjLength += 2;
-                }
-            }
-
-            requiredCapacity += adjLength;
-        }
-
-        // Don't undercut how much space we need in our subMeshList if additions were made
-        if (subMeshList[targetIndex].Capacity < requiredCapacity)
-        {
-            subMeshList[targetIndex].Capacity = requiredCapacity;
-        }
-    }
-
-    public void AddSingleMesh(MeshInstance instance)
-    {
-        // The number of vertices is needed to add the mesh.  This is a little faster than calling Count multiple times
-        int vertexCount = vertexList.Count;
-
-        VertexStart(instance.mesh.vertexCount);
-
-        // Get the mesh's info
-        uv.AddRange(instance.mesh.uv);
-        uv2.AddRange(instance.mesh.uv2);
-        colourList.AddRange(instance.mesh.colors);
-
-        vertexList.AddRange(instance.mesh.vertices.Select(verts => instance.transform.MultiplyPoint(verts)));
-        meshNormals.AddRange(instance.mesh.normals.Select(norms => instance.transform.inverse.transpose.MultiplyVector(norms).normalized));
-        tangentList.AddRange(instance.mesh.tangents.Select(tans => // More than a function call!  See within the breackets below
-        {
-            Vector3 pos = new Vector3(tans.x, tans.y, tans.z);
-            pos = instance.transform.inverse.transpose.MultiplyVector(pos).normalized;
-            return new Vector4(pos.x, pos.y, pos.z, tans.w);
-        }));
-
-        // If there's no triangle list, then we're relying more on our subMesh list
-        if (isTriangles)
-        {
-            int[] input = instance.mesh.GetTriangles(instance.subMeshIndex);
-            AddMeshStart(instance.targetIndex, new[] { input.Length });
-            List<int> output = subMeshList[instance.targetIndex];
-
-            // Add mesh if one is there
-            if (output.Count != 0)
-            {
-                output.Add(output[output.Count - 1]);
-                output.Add(input[0] + vertexCount);
-
-                // Some meshes take up a little extra space
-                if ((output.Count & 1) == 1)
-                {
-                    output.Add(input[0] + vertexCount);
-                }
-            }
-
-            // Adjust mesh vertices
-            output.AddRange(input.Select(verts => verts + vertexCount));
-        }
-        else // Just use the triangle list
-        {
-            int[] trianglesToAdd = instance.mesh.GetTriangles(instance.subMeshIndex);
-            TriangleStart(instance.targetIndex, trianglesToAdd.Length);
-            triangleList[instance.targetIndex].AddRange(trianglesToAdd.Select(tris => tris + vertexCount));
-        }
-    }
-
-    public void AddMultipleMeshes(IEnumerable<MeshInstance> instances)
-    {
-        // We can't just simply call AddSingleMesh multiple times.  We have to take into account that there could be a mix of differing vertexLists & triangleLists
-        instances = instances.Where(instance => instance.mesh);
-        VertexStart(instances.Sum(instance => instance.mesh.vertexCount));
-
-        foreach (IGrouping<int, MeshInstance> subMeshIterator in instances.GroupBy(instance => instance.targetIndex))
-        {
-            if (isTriangles)
-            {
-                AddMeshStart(subMeshIterator.Key, subMeshIterator.Select(instance => instance.mesh.GetTriangles(instance.subMeshIndex).Length));
-            }
-            else
-            {
-                TriangleStart(subMeshIterator.Key, subMeshIterator.Sum(instance => instance.mesh.GetTriangles(instance.subMeshIndex).Length));
-            }
-        }
-
-        foreach (MeshInstance instance in instances)
-        {
-            AddSingleMesh(instance);
-        }
-    }
-
-    public Mesh CreateCombine()
-    {
-        // Define the mesh we'll end up making
-        Mesh mesh = new Mesh
-        {
-            name = "Combine",
-            vertices = vertexList.ToArray(),
-            normals = meshNormals.ToArray(),
-            colors = colourList.ToArray(),
-            uv = uv.ToArray(),
-            uv2 = uv2.ToArray(),
-            tangents = tangentList.ToArray(),
-            subMeshCount = (isTriangles) ?subMeshList.Count : triangleList.Count
-        };
-
-        if (isTriangles)
-        {
-            foreach (var subMeshIterator in subMeshList)
-                mesh.SetTriangles(subMeshIterator.Value.ToArray(), subMeshIterator.Key);
+          adjLength += 3;
         }
         else
         {
-            foreach (var subMeshIterator in triangleList)
-                mesh.SetTriangles(subMeshIterator.Value.ToArray(), subMeshIterator.Key);
+          adjLength += 2;
         }
+      }
 
-        return mesh;
+      requiredCapacity += adjLength;
     }
 
-    public static Mesh CombinePrep(IEnumerable<MeshInstance> instances, bool trianglesOrList)
+    // Don't undercut how much space we need in our subMeshList if additions were made
+    if (subMeshList[targetIndex].Capacity < requiredCapacity)
     {
-        // This will create a prepared class object for combining once the user is ready
-        NewMeshInstantiate processor = new NewMeshInstantiate(trianglesOrList);
-        processor.AddMultipleMeshes(instances);
-        return processor.CreateCombine();
+      subMeshList[targetIndex].Capacity = requiredCapacity;
     }
+  }
+
+  public void AddSingleMesh(MeshInstance instance)
+  {
+    // The number of vertices is needed to add the mesh.  This is a little faster than calling Count multiple times
+    int vertexCount = vertexList.Count;
+
+    VertexStart(instance.mesh.vertexCount);
+
+    // Get the mesh's info
+    uv.AddRange(instance.mesh.uv);
+    uv2.AddRange(instance.mesh.uv2);
+    colourList.AddRange(instance.mesh.colors);
+
+    vertexList.AddRange(instance.mesh.vertices.Select(verts => instance.transform.MultiplyPoint(verts)));
+    meshNormals.AddRange(instance.mesh.normals.Select(norms => instance.transform.inverse.transpose.MultiplyVector(norms).normalized));
+    tangentList.AddRange(instance.mesh.tangents.Select(tans => // More than a function call!  See within the breackets below
+    {
+      Vector3 pos = new Vector3(tans.x, tans.y, tans.z);
+      pos = instance.transform.inverse.transpose.MultiplyVector(pos).normalized;
+      return new Vector4(pos.x, pos.y, pos.z, tans.w);
+    }));
+
+    // If there's no triangle list, then we're relying more on our subMesh list
+    if (isTriangles)
+    {
+      int[] input = instance.mesh.GetTriangles(instance.subMeshIndex);
+      AddMeshStart(instance.targetIndex, new[] { input.Length });
+      List<int> output = subMeshList[instance.targetIndex];
+
+      // Add mesh if one is there
+      if (output.Count != 0)
+      {
+        output.Add(output[output.Count - 1]);
+        output.Add(input[0] + vertexCount);
+
+        // Some meshes take up a little extra space
+        if ((output.Count & 1) == 1)
+        {
+          output.Add(input[0] + vertexCount);
+        }
+      }
+
+      // Adjust mesh vertices
+      output.AddRange(input.Select(verts => verts + vertexCount));
+    }
+    else // Just use the triangle list
+    {
+      int[] trianglesToAdd = instance.mesh.GetTriangles(instance.subMeshIndex);
+      TriangleStart(instance.targetIndex, trianglesToAdd.Length);
+      triangleList[instance.targetIndex].AddRange(trianglesToAdd.Select(tris => tris + vertexCount));
+    }
+  }
+
+  public void AddMultipleMeshes(IEnumerable<MeshInstance> instances)
+  {
+    // We can't just simply call AddSingleMesh multiple times.  We have to take into account that there could be a mix of differing vertexLists & triangleLists
+    instances = instances.Where(instance => instance.mesh);
+    VertexStart(instances.Sum(instance => instance.mesh.vertexCount));
+
+    foreach (IGrouping<int, MeshInstance> subMeshIterator in instances.GroupBy(instance => instance.targetIndex))
+    {
+      if (isTriangles)
+      {
+        AddMeshStart(subMeshIterator.Key, subMeshIterator.Select(instance => instance.mesh.GetTriangles(instance.subMeshIndex).Length));
+      }
+      else
+      {
+        TriangleStart(subMeshIterator.Key, subMeshIterator.Sum(instance => instance.mesh.GetTriangles(instance.subMeshIndex).Length));
+      }
+    }
+
+    foreach (MeshInstance instance in instances)
+    {
+      AddSingleMesh(instance);
+    }
+  }
+
+  public Mesh CreateCombine()
+  {
+    // Define the mesh we'll end up making
+    Mesh mesh = new Mesh
+    {
+      name = "Combine",
+      vertices = vertexList.ToArray(),
+      normals = meshNormals.ToArray(),
+      colors = colourList.ToArray(),
+      uv = uv.ToArray(),
+      uv2 = uv2.ToArray(),
+      tangents = tangentList.ToArray(),
+      subMeshCount = (isTriangles) ? subMeshList.Count : triangleList.Count
+    };
+
+    if (isTriangles)
+    {
+      foreach (var subMeshIterator in subMeshList)
+        mesh.SetTriangles(subMeshIterator.Value.ToArray(), subMeshIterator.Key);
+    }
+    else
+    {
+      foreach (var subMeshIterator in triangleList)
+        mesh.SetTriangles(subMeshIterator.Value.ToArray(), subMeshIterator.Key);
+    }
+
+    return mesh;
+  }
+
+  public static Mesh CombinePrep(IEnumerable<MeshInstance> instances, bool trianglesOrList)
+  {
+    // This will create a prepared class object for combining once the user is ready
+    NewMeshInstantiate processor = new NewMeshInstantiate(trianglesOrList);
+    processor.AddMultipleMeshes(instances);
+    return processor.CreateCombine();
+  }
 }
