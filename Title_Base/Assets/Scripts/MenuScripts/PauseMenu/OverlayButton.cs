82a60617ff65f1a53eeb95d4f9c1e75c652182f8
Steven Gallwas <s.gallwas@digipen.edu>
2015-11-20 10:41:18 -0800

    Improving new main menu.
    Still need to get the overlay working.

---
+++ Title_Base/Assets/Scripts/MenuScripts/PauseMenu/OverlayButton.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class OverlayButton : MenuButton
+{
+  public GameObject Overlay = null;
+
+  public override void Activate()
+  {
+
+  }
+
+  // Update is called once per frame
+  void Update()
+  {
+
+  }
+}
