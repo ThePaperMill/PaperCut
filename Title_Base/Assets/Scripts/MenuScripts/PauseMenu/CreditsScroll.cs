5ce306cb7d1b2ad905a2558979dae500b65bb663
Steven Gallwas <s.gallwas@digipen.edu>
2015-11-19 20:00:09 -0800

    Adding additional menu scripts
    adding splash screen, and credits scroll scripts

---
+++ Title_Base/Assets/Scripts/MenuScripts/PauseMenu/CreditsScroll.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CreditsScroll : MonoBehaviour 
+{
+  public float ScrollSpeed = 25.0f;
+
+  float Timer = 0.0f;
+
+  public float Delay = 30.0f;
+
+	// Use this for initialization
+	void Start () 
+  {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () 
+  {
+    if (Timer > Delay)
+      Application.LoadLevel("MainMenu");
+
+    Timer += Time.deltaTime;
+
+    print(Timer);
+      transform.position += Vector3.up * ScrollSpeed * Time.deltaTime;
+	}
+}
---
+++ Title_Base/Assets/Scripts/MenuScripts/PauseMenu/SplashScreenLogic.cs
@@ -0,0 +1,48 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+
+public class SplashScreenLogic : MonoBehaviour 
+{
+  public string LevelToLoad = "";
+
+  public List<Sprite> Sprites = new List<Sprite>();
+
+  public float ScreenTime = 3.0f;
+
+  float TransitionTimer = 0.0f;
+
+  int CurImage = 0;
+
+  Image SplashImage = null;
+
+	// Use this for initialization
+	void Start () 
+  {
+    SplashImage = GetComponent<Image>();
+
+	}
+	
+	// Update is called once per frame
+	void Update () 
+  {
+    TransitionTimer += Time.deltaTime;
+
+    if (TransitionTimer > ScreenTime)
+    {
+      ++CurImage;
+      TransitionTimer = 0.0f;
+
+      if (CurImage >= Sprites.Count)
+      {
+        // load level 
+        Application.LoadLevel(LevelToLoad);
+      }
+      else
+      {
+        SplashImage.overrideSprite = Sprites[CurImage];
+      }
+    }
+	}
+}
