22f125d41da238dd0f0cae5b14b4bf8285271150
jnac <j.nacier@digipen.edu>
2016-04-10 22:05:31 -0700

    PuppyScript
    Only thing missing is The random velocity and locking the puppies'
    rotations so they don't fall over upon spawning

---
+++ Title_Base/Assets/ArtAssets/Characters/Puppy/SpawnPuppies.cs
@@ -0,0 +1,46 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SpawnPuppies : MonoBehaviour
+{
+
+    public bool PuppiesAreSpawned = false;
+
+    public GameObject Puppy = null;
+
+    public int PuppyCounter = 1;
+
+    public Transform SpawnLocation = null;
+
+	// Use this for initialization
+	void Start ()
+    {
+        gameObject.Connect(Events.TabUpdatedEvent, OnTabUpdated);
+	}
+	
+    public void OnTabUpdated(EventData data)
+    {
+        var TestData = data as FloatEvent;
+        //print(TestData.value); Value is from 1 to almost 0
+
+        if (TestData.value < 0.05 && PuppiesAreSpawned == false)
+        {
+            PuppyCannon();
+            PuppiesAreSpawned = true;
+        }
+    }
+
+	// Update is called once per frame
+	void Update ()
+    {
+	}
+
+    public void PuppyCannon()
+    {
+        for(int i = 0; i < PuppyCounter; ++i)
+        {
+            var puppy = GameObject.Instantiate(Puppy, SpawnLocation.position, Quaternion.identity);
+        }
+        
+    }
+}
