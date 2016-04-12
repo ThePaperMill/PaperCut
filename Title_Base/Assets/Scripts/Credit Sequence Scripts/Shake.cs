3e3a74569f3ed88da6f57c6b9b16d73980f05a61
IanAemmer <iaemmer10@gmail.com>
2016-04-12 14:47:06 -0700

    Finished Ending Sequence (besides audio)

--- Title_Base/Assets/Scripts/Credit Sequence Scripts/Shake.cs
+++ Title_Base/Assets/Scripts/Credit Sequence Scripts/Shake.cs
@@ -19,63 +19,23 @@ public class Shake : MonoBehaviour {
 
     public bool decelerating = false;
 
+    public float turnrate = 10;
+
     Vector3 StartPos = Vector3.zero;
     bool Circular = false;
     
     // Use this for initialization
     void Start () {
-        StartPos = transform.position;
+        StartPos = transform.localPosition;
 	}
 
 
     void Update()
     {
 
-
-        //Slowly increment my amount (distance for circular motion) to a max level
-        //amount += 0.01f;
-
-
-
-        /*
-        if(Circular)
-        {
-
-
-            if (circstart >= 1)
-            {
-                decelerating = true;
-
-            }
-            if(decelerating)
-            {
-                circstart -= Time.deltaTime / 5;
-                if (circstart <= 0)
-                {
-                    circstart = 0;
-                }
-            }
-            else
-            {
-                circstart += Time.deltaTime / 5;
-            }
-
-            amount = Mathf.Lerp(0, 1f, circstart);
-
-            //Apply circular rotation 
-            transform.position = new Vector3(StartPos.x + Mathf.Sin(Time.time * amount), StartPos.y + Mathf.Cos(Time.time * amount), 0) * amount * 2;
-            transform.position += new Vector3(0, 0, StartPos.z);
-
             //I need to apply the shake
-            Vector2 v2 = Random.insideUnitCircle * speed / 10;
-            transform.position = transform.localPosition + new Vector3(v2.x, v2.y, 0);
-            //I want to space myself. my space from 
-        }*///
-        //else
-        //{
-            //I need to apply the shake
-            Vector2 v2 = Random.insideUnitCircle * speed / 10;
-            transform.position = StartPos + new Vector3(v2.x, v2.y, StartPos.z);
+            Vector2 v2 = Random.insideUnitCircle * speed / turnrate;
+            transform.localPosition = StartPos + new Vector3(v2.x, v2.y, 0);
 
             speed -= Time.deltaTime / 5;
             if (speed <= 0)
