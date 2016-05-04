20619248b858319f262cfb876eb1175881936fac
IanAemmer <iaemmer10@gmail.com>
2016-05-03 21:31:53 -0700

    Updated Arrow Prefabs Updated Wobble Script Updated Grass prefabs Updated Fence Prefabs

---
+++ Title_Base/Assets/LerpBackAndForth.cs
@@ -0,0 +1,58 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LerpBackAndForth : MonoBehaviour {
+
+    public float timelength = 5.0f;
+    public AnimationCurve Curve = AnimationCurve.EaseInOut(0, 0, 1, 1);
+    public Vector3 endPos = Vector3.zero;
+    Vector3 startPos = Vector3.zero;// = transform.position;
+
+    // Use this for initialization
+    void Start () {
+        startPos = transform.localPosition;
+        //I need to have an ease curve and josh's animation system
+
+        //endPos = transform.InverseTransformDirection(endPos);
+        StartCoroutine("LerpUp");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    IEnumerator LerpUp()
+    {
+        //transform.InverseTransformDirection(Vector3.forward);
+
+        //float elapsedTime = 0f;
+
+        for (float elapsedTime = 0f; elapsedTime < timelength; elapsedTime += Time.deltaTime)
+        {
+            transform.localPosition = Vector3.Lerp(startPos, endPos, Curve.Evaluate(elapsedTime / timelength));
+            yield return null;
+        }
+
+       // yield return new WaitForSeconds(timelength);
+        print("yes");
+
+        StartCoroutine("LerpDown");
+    }
+    IEnumerator LerpDown()
+    {
+
+        //float elapsedTime = 0f;
+
+        for (float elapsedTime = 0f; elapsedTime < timelength; elapsedTime += Time.deltaTime)
+        {
+            transform.localPosition = Vector3.Lerp( endPos, startPos, Curve.Evaluate(elapsedTime / timelength));
+            yield return null;
+        }
+
+        // yield return new WaitForSeconds(timelength);
+        print("yes");
+
+        StartCoroutine("LerpUp");
+    }
+}
--- Title_Base/Assets/WiggleOnHit.cs
+++ Title_Base/Assets/WiggleOnHit.cs
@@ -1,15 +1,38 @@
 ﻿using UnityEngine;
 using System.Collections;
 
+[RequireComponent(typeof(Rigidbody))]
+[RequireComponent(typeof(HingeJoint))]
 public class WiggleOnHit : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
+
+    Rigidbody rig = null;
+    public float force = 1000f;
+
+
+    // Use this for initialization
+    void Start () {
 	
+        rig = gameObject.GetComponent<Rigidbody>();
 	}
 	
 	// Update is called once per frame
 	void Update () {
 	
 	}
+
+    void OnTriggerEnter(Collider col)
+    {
+        if(col.tag == "Player")
+        {
+            //print("I hit the player working" + Time.time);
+
+            //get the direction from the player
+            Vector3 dir = transform.position - col.gameObject.transform.position;
+
+            //add a force from the player towards the plant
+            rig.AddForce(dir*1000);
+            //asdgasdhol
+        }
+    }
 }
