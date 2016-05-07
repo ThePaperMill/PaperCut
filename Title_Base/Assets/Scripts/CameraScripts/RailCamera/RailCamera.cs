8cb7bfc6c705a4db39482cba93afc536dd9bce96
MRVanZant <mikevanzant@gmail.com>
2016-05-07 16:32:16 -0700

    Rail Camera added: see description for instructions
    Delete the old main camera, and instead use the RailCamera prefab. It
    has 3 settings: Rail, Speed, and Look Direction.
    Rail is the rail the camera will stick to.
    Speed is the Lerp Speed (0 is no movement, 100 is match the player)
    Look direction is the rotation (in euler angles) of the camera.
    
    Use the Rail prefab to insert a 2 node rail. The camera will stay on a
    point between these two nodes. You can duplicate nodes, and it will form
    a rail between any duplicates as well. If A-B-C, A goes to B and B goes
    to C, but A does not go to C. If C-A-B, C goes to A, A goes to B, but C
    does not go to B.
    
    Rail Camera Regions can move to a different rail, give a new move rate,
    and/or a new look direction. If any of these are left at the default
    value, they will be ignored. Once the player leaves the region, these
    will reset to the defaults given on the camera.

---
+++ Title_Base/Assets/Scripts/CameraScripts/RailCamera/RailCamera.cs
@@ -0,0 +1,47 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RailCamera : MonoBehaviour
+{
+    public Rail DefaultRail;
+    public float DefaultMoveSpeed = 2f;
+    public Vector3 DefaultLookAngle = new Vector3(20,0,0);
+
+    [HideInInspector]
+    public Rail CurrentRail;
+    [HideInInspector]
+    public float CurrentMoveSpeed;
+
+    private GameObject TargetObject;         // The object we want the camera to focus on
+    private GameObject DefaultTarget;       // Variable for storing default value of TargetObject
+
+    // Use this for initialization
+    void Start ()
+    {
+        CurrentRail = DefaultRail;
+        CurrentMoveSpeed = DefaultMoveSpeed;
+        transform.eulerAngles = DefaultLookAngle;
+    }
+	
+	// Update is called once per frame
+	public void Update ()
+    {
+        if (TargetObject == null)
+        {
+            TargetObject = GameObject.FindGameObjectWithTag("Player");
+
+            DefaultTarget = TargetObject;       // Store the default value of TargetObject
+
+            return;
+        }
+
+        transform.position = Vector3.Lerp(transform.position, CurrentRail.RailPosition(TargetObject.transform.position), Time.deltaTime * CurrentMoveSpeed);
+    }
+
+    public void ReturnToDefaults()
+    {
+        CurrentRail = DefaultRail;
+        CurrentMoveSpeed = DefaultMoveSpeed;
+        transform.eulerAngles = DefaultLookAngle;
+    }
+}
---
+++ Title_Base/Assets/Scripts/CameraScripts/RailCamera/RailCameraRegion.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RailCameraRegion : MonoBehaviour
+{
+    public Rail NewRail;
+    public float NewSpeed;
+    public Vector3 NewAngle;
+
+    void OnTriggerEnter(Collider otherObj)
+    {
+        var cam = Camera.main.GetComponent<RailCamera>();
+
+        // If a new Target is specified, change it on the main camera
+        if (NewRail != null)
+        {
+            cam.CurrentRail = NewRail;
+        }
+        // If a new Distance is specified, change it on the main camera
+        if (NewAngle != new Vector3(0, 0, 0))
+        {
+            Camera.main.transform.eulerAngles = NewAngle;
+        }
+        // if a new LerpSpeed is specified, change it on the main camera.
+        if (NewSpeed != 0.0f)
+        {
+            cam.CurrentMoveSpeed = NewSpeed;
+        }
+    }
+
+    void OnTriggerExit(Collider otherObj)
+    {
+        var cam = Camera.main.GetComponent<RailCamera>();
+
+        cam.ReturnToDefaults();
+    }
+}
